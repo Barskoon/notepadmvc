256bb88cf200ed0ed3053e790bc8cd3c10e17d45
TilekTekinov <tilektekinov@gmail.com>
2021-11-02 15:18:21 +0600

    TextLineNumber moved to utils

--- Viewer.java
+++ Viewer.java
@@ -3,6 +3,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 import java.io.File;
+import utils.TextLineNumber;
 
 public class Viewer {
 
--- utils/TextLineNumber.java
+++ utils/TextLineNumber.java
@@ -1,3 +1,5 @@
+package utils;
+
 import java.awt.*;
 import java.awt.event.*;
 import java.beans.*;
