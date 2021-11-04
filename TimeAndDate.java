afd4498165dbfd54f7792c339112df2e681313ce
Артём Ильин <exp13a@gmail.com>
2021-11-04 21:54:04 +0600

    Add Select All, Time and Date. Fixing bugs with printing. Add images and more.

---
+++ TimeAndDate.java
@@ -0,0 +1,26 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class TimeAndDate implements Task {
+    private Viewer viewer;
+    private DateFormat dateFormat;
+    private Date date;
+
+    public TimeAndDate(Viewer viewer) {
+        this.viewer = viewer;
+        dateFormat = new SimpleDateFormat("HH:mm:ss dd/MM/yyyy");
+        date = new Date();
+    }
+
+    public void doTask() {
+        int resultCursorPosition = viewer.getCursorPosition();
+        String textForInsertDate = viewer.getInputText();
+        StringBuilder stringBuilder = new StringBuilder(textForInsertDate);
+
+        stringBuilder.insert(resultCursorPosition, dateFormat.format(date));
+
+        viewer.updateText(stringBuilder.toString());
+        stringBuilder.setLength(0);
+    }
+}
