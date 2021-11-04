8e2516127488f3ef080959778321b88c119bc645
Артём Ильин <exp13a@gmail.com>
2021-11-04 22:22:50 +0600

    Fix merge conflicts

---
+++ SelectAll.java
@@ -0,0 +1,16 @@
+public class SelectAll implements Task {
+    private Viewer viewer;
+
+    public SelectAll(Viewer viewer) {
+        this.viewer = viewer;
+    }
+
+    public void doTask() {
+        String textForSelect = viewer.getInputText();
+        if (!textForSelect.equals("")) {
+            viewer.selectAllText();
+        } else {
+            viewer.showMessage("Input text is empty!");
+        }
+    }
+}
